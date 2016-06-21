7070d70bb889faf309444a36778ef577270fd578
Matt Fraser <fraser.matt.a@gmail.com>
2016-06-21 16:20:28 -0700

    Changed our secret key

--- src/plantalytics_backend/settings.py
+++ src/plantalytics_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u5tc0d^8h%s@@lbw(pd*8f1l49w(f)bbi8x2pl8*!-2$2mu2%!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

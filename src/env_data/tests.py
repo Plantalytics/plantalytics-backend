0b61c58fbf6023f5cc0d6a403096e10a65c2f6c8
Matt Fraser <fraser.matt.a@gmail.com>
2016-06-21 22:34:39 -0700

    Added some unit tests

--- src/env_data/tests.py
+++ src/env_data/tests.py
@@ -6,8 +6,20 @@ from django.test.utils import setup_test_environment
 class MainTests(TestCase):
 
 
-    def test_response_data(self):
+    def test_response_temperature_data(self):
         setup_test_environment()
         client = Client()
         response = client.get('/env_data/0/temperature/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_response_humidity_data(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/env_data/0/humidity/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_response_leafwetness_data(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/env_data/0/leafwetness/')
         self.assertEqual(response.status_code, 200)

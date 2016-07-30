aab37d321bb3ad15bc889dd5c18e4a0d7af1875e
Scott Ewing <ewing@pdx.edu>
2016-07-30 12:49:46 -0700

    Added url pattern

--- src/plantalytics_backend/urls.py
+++ src/plantalytics_backend/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     url(r'^env_data', include('env_data.urls')),
     url(r'^hub_data', include('hub_data.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login', include('login.urls'))
+    url(r'^login', include('login.urls')),
+    url(r'^password/', include('password.urls'))
 ]

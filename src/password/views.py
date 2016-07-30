f2451555fd320bf1f279b3038be3ee7d90795aec
Matt Fraser <fraser.matt.a@gmail.com>
2016-07-30 14:27:08 -0700

    Added Cassy support for retrieving user email

--- src/password/views.py
+++ src/password/views.py
@@ -41,4 +41,33 @@ def change(request):
 
 @csrf_exempt
 def reset(request):
+    """
+    Endpoint to email a password reset token to the requested user.
+    """
+    if request.method != 'POST':
+        return HttpResponseBadRequest()
+
+    data = json.loads(request.body.decode('utf-8'))
+    username = data.get('username', '')
+
+    try:
+        email_object = {}
+        email_object['email'] = cassy.get_user_email(username)
+        return HttpResponse(json.dumps(email_object), content_type='application/json')
+    # Invalid username -- expected exception
+    except PlantalyticsException as e:
+        logger.warning('Unknown username \''
+                       + username + '\'.')
+        error = custom_error(str(e))
+        return HttpResponseForbidden(error, content_type='application/json')
+    # Unexpected exception
+    except Exception as e:
+        logger.exception('Error occurred while fetching email for user \''
+                         + username + ' \'.'
+                         + str(e)
+                         )
+        error = custom_error(EMAIL_ERROR, str(e))
+        return HttpResponseForbidden(error, content_type='application/json')
+
+
     return HttpResponse()

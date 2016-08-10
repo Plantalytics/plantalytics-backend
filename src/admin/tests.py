743b31059ac478fe569db28906503d5a4fa83342
Matt Fraser <fraser.matt.a@gmail.com>
2016-08-09 23:08:45 -0700

    Added unit tests

--- src/admin/tests.py
+++ src/admin/tests.py
@@ -7,6 +7,57 @@
 # Contact: plantalytics.capstone@gmail.com
 #
 
-from django.test import TestCase
+import os
+import json
 
-# Create your tests here.
+from common.exceptions import *
+from django.test import TestCase, Client
+from django.test.utils import setup_test_environment
+
+
+class MainTests(TestCase):
+    """
+    Executes all of the unit tests for the admin endpoints.
+    """
+
+    def test_user_info(self):
+        """
+        Tests a valid request for user info.
+        """
+        setup_test_environment()
+        client = Client()
+        payload = {
+            'auth_token': os.environ.get('ADMIN_TOKEN'),
+            'admin_username': os.environ.get('ADMIN_USER'),
+            'request_username': os.environ.get('LOGIN_USERNAME'),
+        }
+        response = client.post(
+            '/admin/user',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        content = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(
+            content.get('email', ''),
+            os.environ.get('RESET_EMAIL')
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_update_sub_date(self):
+        """
+        Tests a valid request to update a user subscription date.
+        """
+        setup_test_environment()
+        client = Client()
+        payload = {
+            'auth_token': os.environ.get('ADMIN_TOKEN'),
+            'admin_username': os.environ.get('ADMIN_USER'),
+            'request_username': os.environ.get('LOGIN_USERNAME'),
+            'sub_end_date': '2100-01-01',
+        }
+        response = client.post(
+            '/admin/user',
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
